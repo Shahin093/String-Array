f1c417fea545dade3e504a085816cfffa0e817ab
Shidul Islam <sishahin093@gmail.com>
2022-02-13 21:25:55 +0600

    29-1 String comparison using toLowerCase toUpperCase

---
+++ case-convert.js
@@ -0,0 +1,7 @@
+const anthem = 'Amar Somar Bangla Ami Tomai Balobasi';
+
+const userInput = "BlackPink";
+const saveUserInput = 'BlackPink';
+if (userInput.toLowerCase() == saveUserInput.toLowerCase()) {
+    console.log("User exits");
+}
